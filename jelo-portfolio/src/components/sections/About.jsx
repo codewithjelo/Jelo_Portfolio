f34451fba6fd4dd322d04c0797fb84fe01b3dd4d
Jelo <jannangelodimaano@gmail.com>
2025-10-21 02:50:01 +0800

    Implement logo loop for the lower part of about section

--- jelo-portfolio/src/components/sections/About.jsx
+++ jelo-portfolio/src/components/sections/About.jsx
@@ -1,23 +1,55 @@
 import AboutCard from "../ui/AboutCard";
 import CertificateCard from "../ui/CertificateCard";
-import '../../styles/components/sections/about.css'
+import TechStackCard from "../ui/TechStackCard";
+import LogoLoop from "../animations/LogoLoop";
+import "../../styles/components/sections/about.css";
+import { SiReact, SiLaravel, SiPython, SiTailwindcss } from "react-icons/si";
 
 const About = () => {
+  const techLogos = [
+    { node: <SiReact />, title: "React" },
+    { node: <SiLaravel />, title: "Laravel"},
+    {
+      node: <SiPython />,
+      title: "Python"
+    },
+    {
+      node: <SiTailwindcss />,
+      title: "Tailwind CSS",
+    },
+  ];
+
   return (
-    <section
-      id="about"
-      className="min-h-screen min-w-screen py-20"
-    >
+    <section id="about" className="min-h-screen min-w-screen py-20">
       <div className="max-w-7xl px-6">
-        <h1 className="tagline uppercase text-center mb-10">“<span>Learning </span> today <span>to build a better</span> tomorrow ”</h1>
+        <h1 className="tagline uppercase text-center mb-10">
+          “<span>Learning </span> today <span>to build a better</span> tomorrow
+          ”
+        </h1>
 
         {/* Content Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-[8vw]">
+        <div className="grid grid-cols-1 md:grid-cols-[650px_1fr] gap-[2vw]">
           {/* Left Column - Personal Info */}
           <AboutCard />
 
           {/* Right Column - Skills & Stats */}
+          <TechStackCard />
+
           <CertificateCard />
+        </div>
+        <div className="relative overflow-hidden w-full mt-20">
+          <LogoLoop
+            className="logo-loop"
+            logos={techLogos}
+            speed={30}
+            direction="left"
+            logoHeight={48}
+            gap={80}
+            scaleOnHover
+            fadeOut
+            fadeOutColor="var(--primary-color)"
+            ariaLabel="Technology partners"
+          />
         </div>
       </div>
     </section>
