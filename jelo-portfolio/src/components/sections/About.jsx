15b4ea1e2fc23566bb4b66de9e4e8aaf951b3cea
Jelo <jannangelodimaano@gmail.com>
2025-10-20 20:36:33 +0800

    Add min-w-screen and tagline in className attribute

--- jelo-portfolio/src/components/sections/About.jsx
+++ jelo-portfolio/src/components/sections/About.jsx
@@ -1,10 +1,15 @@
 import AboutCard from "../ui/AboutCard";
 import CertificateCard from "../ui/CertificateCard";
+import '../../styles/components/sections/about.css'
 
 const About = () => {
   return (
-    <section id="about" className="min-h-screen py-20">
+    <section
+      id="about"
+      className="min-h-screen min-w-screen py-20"
+    >
       <div className="max-w-7xl px-6">
+        <h1 className="tagline uppercase text-center mb-10">“<span>Learning </span> today <span>to build a better</span> tomorrow ”</h1>
 
         {/* Content Grid */}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-[8vw]">
@@ -13,7 +18,6 @@ const About = () => {
 
           {/* Right Column - Skills & Stats */}
           <CertificateCard />
-          
         </div>
       </div>
     </section>
