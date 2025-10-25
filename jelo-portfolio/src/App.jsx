b90fd497f545bdb20aeb2768b4e036256eabe3e3
Jelo <jannangelodimaano@gmail.com>
2025-10-25 19:38:16 +0800

    Add footer component

--- jelo-portfolio/src/App.jsx
+++ jelo-portfolio/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/layout/Header"
 import Layout from "./components/layout/Layout"
+import Footer from "./components/layout/Footer"
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Layout />
+      <Layout />  
+      <Footer />
     </div>
   );
 }
