271d6e235dde4d34f6d244571223c0c3e2cced2f
Jelo <jannangelodimaano@gmail.com>
2025-11-04 16:59:09 +0800

    Add inline css and modify text size

--- jelo-portfolio/src/components/ui/TechStackCard.jsx
+++ jelo-portfolio/src/components/ui/TechStackCard.jsx
@@ -17,8 +17,8 @@ const TechStackCard = () => {
 
   return (
     <div className="techstack-card p-5">
-      <h2 className="techstack-text text-xl flex font-bold mb-4">
-        <Bolt className="about-icon mr-2" size={24} />
+      <h2 className="techstack-text text-2xl flex font-bold mb-4">
+        <Bolt className="about-icon mt-1 mr-2" size={24} />
         Tech Stack
       </h2>
       <div className="overflow-y-auto max-h-40 md:max-h-full md:overflow-hidden">
