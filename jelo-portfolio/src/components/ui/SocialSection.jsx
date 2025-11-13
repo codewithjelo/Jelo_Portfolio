61eb14c75139ed1ed89ffbd5bdb0694d1b0f6a13
Jelo <jannangelodimaano@gmail.com>
2025-11-14 01:47:23 +0800

    Resize google maps when screen size changed

--- jelo-portfolio/src/components/ui/SocialSection.jsx
+++ jelo-portfolio/src/components/ui/SocialSection.jsx
@@ -70,7 +70,7 @@ const SocialSection = () => {
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d247488.0378237748!2d120.88975685!3d13.756331349999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd05f87640c75b%3A0xf1dd21643215242!2sBatangas%20City%2C%20Batangas!5e0!3m2!1sen!2sph!4v1647000000000!5m2!1sen!2sph"
                         width="300"
                         height="200"
-                        style={{ border: 0 }}
+                        className="google-maps"
                         allowFullScreen=""
                         loading="lazy"
                         referrerPolicy="no-referrer-when-downgrade"
