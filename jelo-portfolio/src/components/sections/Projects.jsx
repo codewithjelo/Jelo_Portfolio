032ea2d19c8462a8715ea1cc00498526a07d051f
Jelo Dimaano <jannangelodimaano@gmail.com>
2026-01-13 11:47:16 +0800

    Add another project

--- jelo-portfolio/src/components/sections/Projects.jsx
+++ jelo-portfolio/src/components/sections/Projects.jsx
@@ -11,20 +11,26 @@ const Projects = forwardRef((props, ref) => {
       id: 1,
       title: "Ibaan Central School Web Portal",
       description:
-        "A full-featured online shopping platform with cart functionality, payment integration, and admin dashboard.",
+        "A full-featured school management platform designed to streamline academic and administrative processes. It provides secure, role-based access for parents, teachers, and administrators, enabling efficient student record management, school announcements, and data-driven analytics dashboards that visualize students’ quarterly academic performance. The system emphasizes usability, transparency, and secure data handling to support informed decision-making and improved communication across the school community.",
       technologies: ["HTML", "CSS", "Bootstrap", "Chart.js", "PHP", "MySQL"],
       images: [parent1, parent2, principal1],
       github: "https://github.com/codewithjelo/ICS_Web_Portal.git",
       demo: "",
-    }
+    },
+    {
+      id: 2,
+      title: "Job Application Tracker",
+      description:
+        "A centralized job application tracking tool that helps users organize applications, monitor progress, and stay on top of deadlines throughout the job search journey",
+      technologies: ["React", "TailwindCSS"],
+      images: [parent1, parent2, principal1],
+      github: "https://github.com/codewithjelo/Job-Application-Tracker.git",
+      demo: "",
+    },
   ];
 
   return (
-    <section
-      id="projects"
-      ref={ref}
-      className="xl:min-h-screen min-w-screen"
-    >
+    <section id="projects" ref={ref} className="xl:min-h-screen min-w-screen">
       <div className="container max-w-full">
         {/* Project Header */}
         <div className="mb-12 text-center">
@@ -39,10 +45,7 @@ const Projects = forwardRef((props, ref) => {
         </div>
 
         {/* Projects Container Card - Scrollable */}
-        <ProjectContainer
-          projects={projects}
-          length={projects.length}
-        />
+        <ProjectContainer projects={projects} length={projects.length} />
       </div>
     </section>
   );
