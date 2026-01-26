39759ee238e6c1e70dea7c67eb6392abb1c86bb8
Jelo Dimaano <jannangelodimaano@gmail.com>
2026-01-26 18:16:42 +0800

    Add new project to feature projects

--- jelo-portfolio/src/components/sections/Projects.jsx
+++ jelo-portfolio/src/components/sections/Projects.jsx
@@ -1,8 +1,11 @@
 import { forwardRef } from "react";
 import "../../styles/components/sections/projects.css";
-import parent1 from "../../assets/images/parent-1.png";
-import parent2 from "../../assets/images/parent-2.png";
-import principal1 from "../../assets/images/principal-1.png";
+import project1_1 from "../../assets/images/project1-1.png";
+import project1_2 from "../../assets/images/project1-2.png";
+import project1_3 from "../../assets/images/project1-3.png";
+import project2_1 from "../../assets/images/project2-1.png";
+import project2_2 from "../../assets/images/project2-2.png";
+import project2_3 from "../../assets/images/project2-3.png";
 import ProjectContainer from "../ui/ProjectContainer";
 
 const Projects = forwardRef((props, ref) => {
@@ -13,19 +16,18 @@ const Projects = forwardRef((props, ref) => {
       description:
         "A full-featured school management platform designed to streamline academic and administrative processes. It provides secure, role-based access for parents, teachers, and administrators, enabling efficient student record management, school announcements, and data-driven analytics dashboards that visualize students’ quarterly academic performance. The system emphasizes usability, transparency, and secure data handling to support informed decision-making and improved communication across the school community.",
       technologies: ["HTML", "CSS", "Bootstrap", "Chart.js", "PHP", "MySQL"],
-      images: [parent1, parent2, principal1],
+      images: [project1_1, project1_2, project1_3],
       github: "https://github.com/codewithjelo/ICS_Web_Portal.git",
       demo: "",
     },
     {
       id: 2,
-      title: "Job Application Tracker",
-      description:
-        "A centralized job application tracking tool that helps users organize applications, monitor progress, and stay on top of deadlines throughout the job search journey",
+      title: "TrabaLog - Job Application Tracker",
+      description: "A web-based system that helps users manage and track their job applications. Users can add and update applications, edit job statuses, and view overall application details. It has visual insights into application counts, status distribution, trends over time, and recent activities, making it easier to monitor job search progress efficiently.",
       technologies: ["React", "TailwindCSS"],
-      images: [parent1, parent2, principal1],
+      images: [project2_1, project2_2, project2_3],
       github: "https://github.com/codewithjelo/Job-Application-Tracker.git",
-      demo: "",
+      demo: "https://trabalog.vercel.app",
     },
   ];
 
